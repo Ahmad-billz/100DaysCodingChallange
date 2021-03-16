71079798b89fc004740e85f26526416327077604
Ahmad-billz <ahmadbilalsohail@hotmail.com>
2021-03-16 21:25:41 +0100

    twilio api, newsapi, alphavantage api

--- stock-news__intermediate+/config.py
+++ stock-news__intermediate+/config.py
@@ -1,6 +1,6 @@
 s_key = '80VGA3DMICIP5XI0'
 n_key = '172bac8bfd544cf197392bd05e404022'
 t_sid = 'AC26a4ed9fcee34e0475897bc2048c5e2d'
-t_at = '6102e99bc4e92c9eac461663050372a0'
+t_at = 'AC26a4ed9fcee34e0475897bc2048c5e2d'
 number = "+4917640564230"
 f_number = "+18587712969"
